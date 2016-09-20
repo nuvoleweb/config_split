e48ed666e1a23fe7ad521fd9cecb7f79e2a84d06
Fabian Bircher <f.bircher@gmail.com>
2016-09-20 17:43:03 +0200

    Issue #2803147: Switch from settings config object to config entities

---
+++ src/Entity/ConfigSplitEntity.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\config_split\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\Core\Config\FileStorage;
+
+/**
+ * Defines the Configuration Split Setting entity.
+ *
+ * @ConfigEntityType(
+ *   id = "config_split",
+ *   label = @Translation("Configuration Split Setting"),
+ *   handlers = {
+ *     "list_builder" = "Drupal\config_split\ConfigSplitEntityListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\config_split\Form\ConfigSplitEntityForm",
+ *       "edit" = "Drupal\config_split\Form\ConfigSplitEntityForm",
+ *       "delete" = "Drupal\config_split\Form\ConfigSplitEntityDeleteForm"
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\config_split\ConfigSplitEntityHtmlRouteProvider",
+ *     },
+ *   },
+ *   config_prefix = "config_split",
+ *   admin_permission = "administer site configuration",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/config/development/configuration/config_split/{config_split}",
+ *     "add-form" = "/admin/config/development/configuration/config_split/add",
+ *     "edit-form" = "/admin/config/development/configuration/config_split/{config_split}/edit",
+ *     "delete-form" = "/admin/config/development/configuration/config_split/{config_split}/delete",
+ *     "collection" = "/admin/config/development/configuration/config_split"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "folder",
+ *     "module",
+ *     "theme",
+ *     "blacklist",
+ *     "weight",
+ *   }
+ * )
+ */
+class ConfigSplitEntity extends ConfigEntityBase implements ConfigSplitEntityInterface {
+
+  /**
+   * The Configuration Split Setting ID.
+   *
+   * @var string
+   */
+  protected $id;
+
+  /**
+   * The Configuration Split Setting label.
+   *
+   * @var string
+   */
+  protected $label;
+
+  /**
+   * The folder to export to.
+   *
+   * @var string
+   */
+  protected $folder = '';
+
+  /**
+   * The modules to split.
+   *
+   * @var array
+   */
+  protected $module = [];
+
+  /**
+   * The themes to split.
+   *
+   * @var array
+   */
+  protected $theme = [];
+
+  /**
+   * The explicit configuration to filter out.
+   *
+   * @var string[]
+   */
+  protected $blacklist = [];
+
+  /**
+   * The weight of the configuration when spliting several folders.
+   *
+   * @var int
+   */
+  protected $weight = 0;
+  
+}
---
+++ src/Form/ConfigSplitEntityForm.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Drupal\config_split\Form;
+
+use Drupal\Core\Entity\EntityForm;
+use Drupal\Core\Form\ConfigFormBaseTrait;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class ConfigSplitEntityForm.
+ *
+ * @package Drupal\config_split\Form
+ */
+class ConfigSplitEntityForm extends EntityForm {
+
+  /**
+   * Drupal\Core\Extension\ThemeHandler definition.
+   *
+   * @var \Drupal\Core\Extension\ThemeHandlerInterface
+   */
+//  protected $themeHandler;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form = parent::form($form, $form_state);
+
+    /** @var \Drupal\config_split\Entity\ConfigSplitEntityInterface $config */
+    $config = $this->entity;
+    $form['label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Label'),
+      '#maxlength' => 255,
+      '#default_value' => $config->label(),
+      '#description' => $this->t("Label for the Configuration Split Setting."),
+      '#required' => TRUE,
+    ];
+
+    $form['id'] = [
+      '#type' => 'machine_name',
+      '#default_value' => $config->id(),
+      '#machine_name' => [
+        'exists' => '\Drupal\config_split\Entity\ConfigSplitEntity::load',
+      ],
+      '#disabled' => !$config->isNew(),
+    ];
+
+    $form['folder'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Folder'),
+      '#description' => $this->t('The folder, relative to the Drupal root, to which to save the filtered config.<br/>Configuration related to the "filtered" items below will be split from the main configuration and exported to this folder by <code>drupal config_split:export</code>.'),
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#default_value' => $config->get('folder'),
+    ];
+
+    $module_handler = $this->moduleHandler;
+    $modules = array_map(function ($module) use ($module_handler) {
+      return $module_handler->getName($module->getName());
+    }, $module_handler->getModuleList());
+    $form['module'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Modules'),
+      '#description' => $this->t('Select modules to filter.'),
+      '#options' => $modules,
+      '#size' => 5,
+      '#multiple' => TRUE,
+      '#default_value' => array_keys($config->get('module')),
+    ];
+
+//    $theme_handler = $this->themeHandler;
+//    $themes = array_map(function ($theme) use ($theme_handler) {
+//      return $theme_handler->getName($theme->getName());
+//    }, $theme_handler->listInfo());
+//    $form['theme'] = [
+//      '#type' => 'select',
+//      '#title' => $this->t('Themes'),
+//      '#description' => $this->t('Select themes to filter.'),
+//      '#options' => $themes,
+//      '#size' => 5,
+//      '#multiple' => TRUE,
+//      '#default_value' => array_keys($config->get('theme')),
+//    ];
+
+    $form['blacklist'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Blacklist'),
+      '#description' => $this->t('Select configuration to filter.'),
+      '#options' => array_combine($this->configFactory()->listAll(), $this->configFactory()->listAll()),
+      '#size' => 5,
+      '#multiple' => TRUE,
+      '#default_value' => $config->get('blacklist'),
+    ];
+
+    $form['weight'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Weight'),
+      '#description' => $this->t('The weight to order the splits.'),
+      '#default_value' => $config->get('weight'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // Transform the values from the form to correctly save the entity.
+    $extensions = $this->config('core.extension');
+    $form_state->setValue('module', array_intersect_key($extensions->get('module'), $form_state->getValue('module')));
+//    $form_state->setValue('theme', array_intersect_key($extensions->get('theme'), $form_state->getValue('theme')));
+    $form_state->setValue('blacklist', array_keys($form_state->getValue('blacklist')));
+
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $config_split = $this->entity;
+    $status = $config_split->save();
+
+    switch ($status) {
+      case SAVED_NEW:
+        drupal_set_message($this->t('Created the %label Configuration Split Setting.', [
+          '%label' => $config_split->label(),
+        ]));
+        break;
+
+      default:
+        drupal_set_message($this->t('Saved the %label Configuration Split Setting.', [
+          '%label' => $config_split->label(),
+        ]));
+    }
+    $form_state->setRedirectUrl($config_split->urlInfo('collection'));
+  }
+
+}
