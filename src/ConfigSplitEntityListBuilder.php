6aa192e730c7d11d6d77b24da9e14945e99bd435
Fabian Bircher <f.bircher@gmail.com>
2016-11-26 17:32:37 +0100

    Issue #2830553: indicate overwritten status on overview page

--- src/ConfigSplitEntityListBuilder.php
+++ src/ConfigSplitEntityListBuilder.php
@@ -2,13 +2,51 @@
 
 namespace Drupal\config_split;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ConfigManagerInterface;
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a listing of Configuration Split Setting entities.
  */
 class ConfigSplitEntityListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * The config factory that knows what is overwritten.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * Constructs a new EntityListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, ConfigFactoryInterface $config_factory) {
+    parent::__construct($entity_type, $storage);
+    $this->configFactory = $config_factory;
+  }
 
   /**
    * {@inheritdoc}
@@ -27,6 +65,11 @@ class ConfigSplitEntityListBuilder extends ConfigEntityListBuilder {
     $row['label'] = $entity->label();
     $row['id'] = $entity->id();
     $row['status'] = $entity->status() ? 'active' : 'inactive';
+    $config = $this->configFactory->get('config_split.config_split.' . $entity->id());
+    if ($config->get('status') != $entity->status()) {
+      $row['status'] .= ' (overwritten)';
+    }
+
     return $row + parent::buildRow($entity);
   }
 
