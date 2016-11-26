afe067f67828ae790c1eee0b10dc7f9bea849d1f
Fabian Bircher <f.bircher@gmail.com>
2016-11-26 15:31:14 +0100

    Issue #2830503 by swentel: Change default permisssion

--- src/Entity/ConfigSplitEntity.php
+++ src/Entity/ConfigSplitEntity.php
@@ -23,7 +23,7 @@ use Drupal\Core\Config\FileStorage;
  *     },
  *   },
  *   config_prefix = "config_split",
- *   admin_permission = "administer site configuration",
+ *   admin_permission = "administer configuration split",
  *   entity_keys = {
  *     "id" = "id",
  *     "label" = "label",
