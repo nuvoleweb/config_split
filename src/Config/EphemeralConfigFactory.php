1cfc51c1a9e1518b1432cc9da034cb3a54ad62c2
Fabian Bircher <f.bircher@gmail.com>
2021-09-11 22:33:08 +0200

    Issue #3219993 by bircher: Make EphemeralConfigFactory compatible with Drupal 8

--- src/Config/EphemeralConfigFactory.php
+++ src/Config/EphemeralConfigFactory.php
@@ -2,15 +2,13 @@
 
 namespace Drupal\config_split\Config;
 
-use Drupal\Core\Config\ConfigFactory;
-use Drupal\Core\Config\StorageInterface;
 // Unfortunately Drupal 9.1 expects the Component event dispatcher so to be
 // compatible we have to implement that too. However, in Drupal 9.2 the
 // Contracts event dispatcher is used. And so once that becomes the minimum
 // supported version we can remove all those methods.
 // use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;.
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Config\StorageInterface;
 
 /**
  * Use all the overrides and logic from the factory but skip event dispatching.
@@ -31,7 +29,7 @@ class EphemeralConfigFactory extends ConfigFactory {
    */
   public static function fromService(ConfigFactory $service, StorageInterface $storage): self {
     // Construct the factory with a non-dispatching event dispatcher.
-    $factory = new static($storage, self::eventDispatcher(), $service->typedConfigManager);
+    $factory = new static($storage, $service->eventDispatcher, $service->typedConfigManager);
     // Steal the factory overrides. This only works because we get the service.
     $factory->configFactoryOverrides = $service->configFactoryOverrides;
 
@@ -39,13 +37,21 @@ class EphemeralConfigFactory extends ConfigFactory {
   }
 
   /**
+   * {@inheritdoc}
+   */
+  public function getEditable($name) {
+    throw new \BadMethodCallException(sprintf("The method %s is not allowed", __METHOD__));
+  }
+
+  /**
    * The event dispatcher which doesn't do anything.
    *
-   * @return \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   * @return \Symfony\Contracts\EventDispatcher\EventDispatcherInterface
    *   The event dispatcher.
    */
   protected static function eventDispatcher() {
-    return new class() implements EventDispatcherInterface {
+    // We can use this class once Drupal 9.2 is the minimum supported version.
+    return new class() /* implements EventDispatcherInterface */ {
 
       /**
        * {@inheritdoc}
@@ -61,55 +67,6 @@ class EphemeralConfigFactory extends ConfigFactory {
         }
 
         throw new \TypeError(sprintf('Argument 1 passed to "%s::dispatch()" must be an object, "%s" given.', EventDispatcherInterface::class, is_object($event) ? get_class($event) : gettype($event)));
-      }
-
-      /**
-       * {@inheritdoc}
-       */
-      public function addListener($eventName, $listener, $priority = 0) {
-        throw new \BadMethodCallException(sprintf("The method %s is not allowed", __METHOD__));
-      }
-
-      /**
-       * {@inheritdoc}
-       */
-      public function addSubscriber(EventSubscriberInterface $subscriber) {
-        throw new \BadMethodCallException(sprintf("The method %s is not allowed", __METHOD__));
-      }
-
-      /**
-       * {@inheritdoc}
-       */
-      public function removeListener($eventName, $listener) {
-        throw new \BadMethodCallException(sprintf("The method %s is not allowed", __METHOD__));
-      }
-
-      /**
-       * {@inheritdoc}
-       */
-      public function removeSubscriber(EventSubscriberInterface $subscriber) {
-        throw new \BadMethodCallException(sprintf("The method %s is not allowed", __METHOD__));
-      }
-
-      /**
-       * {@inheritdoc}
-       */
-      public function getListeners($eventName = NULL) {
-        throw new \BadMethodCallException(sprintf("The method %s is not allowed", __METHOD__));
-      }
-
-      /**
-       * {@inheritdoc}
-       */
-      public function getListenerPriority($eventName, $listener) {
-        throw new \BadMethodCallException(sprintf("The method %s is not allowed", __METHOD__));
-      }
-
-      /**
-       * {@inheritdoc}
-       */
-      public function hasListeners($eventName = NULL) {
-        throw new \BadMethodCallException(sprintf("The method %s is not allowed", __METHOD__));
       }
 
     };
